244d5df357a0aa53e1b4de77baf83189ce90a7db
Justin Buchanan <justbuchanan@gmail.com>
2015-11-18 20:44:24 -0500

    exported radio regs

--- firmware/common2015/drivers/cc1201/CC1201Radio.hpp
+++ firmware/common2015/drivers/cc1201/CC1201Radio.hpp
@@ -9,7 +9,8 @@
  * 		-should be an RF studio header export
  * 		-*ALL* registers must be exported
  */
-#include "cc1201_rj_config_920mhz.hpp"
+// #include "cc1201_rj_config_920mhz.hpp"
+#include "semi-working-cc1201-regs.h"
 
 #ifndef SMARTRF_RADIO_CC1201
 #error "A valid Smart RF register configuration file was not loaded."
