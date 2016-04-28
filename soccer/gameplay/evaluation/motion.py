f03f1fa3e78708b7fdf4d1d6e12498840118addb
John Carnahan <JohnGMCarnahan@gmail.com>
2016-04-28 19:59:25 -0400

    Realized that the robot could be moving in a different direction than the point. Currently commiting while I reevaluate my method of prediction

--- soccer/gameplay/evaluation/motion.py
+++ soccer/gameplay/evaluation/motion.py
@@ -8,20 +8,21 @@ max_robot_accel = robocup.MotionContraints.MaxRobotAccel.value()
 
 ## Returns the minimum time to reach the given point for the given robot.
 # This assumes that the robot doesn't slow down to reach the point, that it doesn't need to avoid any obstacles, and it follows our motion constraints
-def timeToPoint(cur_robot, final_point):
+def timeToPoint(cur_velocity, final_point):
 	if cur_robot == None
 		return float("inf")
 	#Distance from robot to evaluation point
 	path_dist = cur_robot.pos.distTo(final_point)
-
+	#Time til max velocity is reached
 	max_v_time = (max_robot_speed - cur_robot.vel.mag()) / max_robot_accel
+	#Distance covered while the robot accelerates to max velocity
 	max_v_dist = .5 * max_robot_accel * max_v_time^2 + cur_robot.vel.mag() * max_v_time
+	#If the robot can reach the point entirely 
 	if path_dist < max_v_dist:
 		#Return time based on the equation: x(t) = .5*a*t^2 + vi*t + xi
-		return (-cur_robot.vel + math.sqrt(cur_robot.vel + 2 * max_robot_accel * path_dist)) / max_robot_accel
+		return (-cur_robot.vel.mag() + math.sqrt(cur_robot.vel.magsq() + 2 * max_robot_accel * path_dist)) / max_robot_accel
 	else:
-		constant_v_time = (path_dist - max_v_dist) / max_robot_speed
-		return max_v_time + constant_v_time
+		return max_v_time + (path_dist - max_v_dist) / max_robot_speed
 
 
 ## Returns the minimum time to reach the ball for the given robot. 
